d4a988723634a8f2ed53b1bc29757ba1c5601d62
NITHYASHREEMURUGESAN <144330969+NITHYASHREEMURUGESAN@users.noreply.github.com>
2023-10-20 14:51:06 +0530

    Update log_code.py

--- Logistic_Regression_With_Neural_Network/log_code.py
+++ Logistic_Regression_With_Neural_Network/log_code.py
@@ -1,3 +1,9 @@
+# 1 - Import Packages
+# numpy is the fundamental package for scientific computing with Python.
+# h5py is a common package to interact with a dataset that is stored on an H5 file.
+# matplotlib is a famous library to plot graphs in Python.
+# PIL and scipy are used here to test your model with your own picture at the end.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
@@ -6,12 +12,12 @@ from PIL import Image
 from scipy import ndimage
 from loading_data import load_dataset
 
+# Loading the dataset
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset("log_dataset")
 index = 25
 plt.imshow(train_set_x_orig[index])
-# print ("y = " + str(train_set_y[:index]) + ", it's a '" + classes[np.squeeze(train_set_y[:index])].decode("utf-8") +  "' picture.")
 
-
+# Its is kept in matrix/vector dimension.
 m_train = train_set_y.shape[0]
 m_test = test_set_y.shape[0]
 num_px = train_set_x_orig.shape[1]
@@ -26,7 +32,7 @@ print ("train_set_y shape: " + str(train_set_y.shape))
 print ("test_set_x shape: " + str(test_set_x_orig.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 
-
+# 'train_set_x_flatten' and 'test_set_x_flatten' are NumPy arrays that are reshaped to be in the form of 2D arrays.
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 ### END CODE HERE ###
@@ -37,7 +43,8 @@ print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
 
-
+#  it scales the pixel values in train_set_x and test_set_x by dividing them by 255. 
+#This is a common preprocessing step for image data, as it scales the pixel values to be in the range of 0 to 1
 train_set_x = train_set_x_flatten / 255.
 test_set_x = test_set_x_flatten / 255.
 
@@ -50,7 +57,8 @@ def sigmoid(z):
 print ("sigmoid(0) = " + str(sigmoid(0)))
 print ("sigmoid(9.2) = " + str(sigmoid(9.2)))
 
-
+#  initialization step for logistic regression or linear models before training them. 
+#It initializes the model's parameters to zero values.
 def initialize_with_zeros(dim):
 
     w = np.zeros(shape=(dim, 1))
@@ -68,7 +76,6 @@ print ("b = " + str(b))
 
 
 # propagate
-
 def propagate(w, b, X, Y):
 
     
@@ -101,6 +108,8 @@ print ("dw = " + str(grads["dw"]))
 print ("db = " + str(grads["db"]))
 print ("cost = " + str(cost))
 
+# the optimization process for logistic regression by updating the parameters to minimize the cost (usually the logistic loss) over a specified number of iterations using gradient descent. 
+#The specific values of w, b, dw, and db will be optimized for the given training data X and labels Y.
 def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
 
     
@@ -145,7 +154,8 @@ print ("b = " + str(params["b"]))
 print ("dw = " + str(grads["dw"]))
 print ("db = " + str(grads["db"]))
 
-
+#  the predict function with the weight vector w, bias b, and input data X, and it prints the resulting predictions. 
+#SThis function is used to apply the trained logistic regression model to new data for classification.
 def predict(w, b, X):
     
     m = X.shape[1]
